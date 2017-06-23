e0771010812820ec1d2cdbcae951466f8d1e9bf7
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-06-23 17:06:21 +0300

    V 2.0.0

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,3 @@
 $(function () {
-  $('.masterTooltip').hover(function(){
-    // Hover over code
-    var title = $(this).attr('data-detail');
-    $(this).data('tipText', title).removeAttr('title');
-    $('<div class="genplan__tooltip"></div>')
-      .html(title)
-      .appendTo('body')
-      .fadeIn(1300);
-  }, function() {
-    // Hover out code
-    $(this).attr('title', $(this).data('tipText'));
-    $('.genplan__tooltip').remove();
-  }).mousemove(function(e) {
-    var mousex = e.pageX - 30; //Get X coordinates
-    var mousey = e.pageY - 110; //Get Y coordinates
-    $('.genplan__tooltip')
-      .css({ top: mousey, left: mousex })
-  });
+
 })
